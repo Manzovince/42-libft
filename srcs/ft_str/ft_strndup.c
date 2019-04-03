2b1cbf59d57e3eaf50ac15bac8b50ea5ffc10687
Manzovince <manzovince@outlook.fr>
2019-04-03 23:01:47 +0200

    test

---
+++ srcs/ft_str/ft_strndup.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmanzoni <vmanzoni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/25 15:02:42 by vmanzoni          #+#    #+#             */
+/*   Updated: 2019/03/25 15:03:12 by vmanzoni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/libft.h"
+
+char	*ft_strndup(const char *s, size_t n)
+{
+	char	*str;
+
+	if (!(str = ft_strnew(n)))
+		return (NULL);
+	str = ft_strncpy(str, (char *)s, n);
+	str[n] = '\0';
+	return (str);
+}
